3516d354f44e785ce7ce3aee7ed351bf327f9b77
Michel Schep <mschep@deloitte.nl>
2014-10-05 11:12:54 +0200

    Het werkt!

--- client/scripts/angular-signalr.js
+++ client/scripts/angular-signalr.js
@@ -1,31 +1,45 @@
-
-var app = angular.module('signalRApp', []);
-
-app.controller('MessagesController', function($scope) {
-    $scope.messages = [{ creationDate: "11:30", body: "Hello! this is a message"}, 
-    { creationDate: "11:33", body: "Hello! this is another message"} ]
-}); 
-
-app.service('breakingNewsService', function ($, $rootScope) {
+angular.module('signalRApp', [])
+.value('$', $)
+.service('breakingNewsService', function ($, $rootScope) {
     var proxy = null;
   
-    var initialize = function () {
+    this.initialize = function () {
+        console.log('create connection');
+
         //Getting the connection object
-        connection = $.hubConnection();
-  
+        var connection = $.hubConnection('http://localhost:8088');
+        connection.logging = true;
+
+        console.log('create hub proxy');
         //Creating proxy
-        this.proxy = connection.createHubProxy('helloWorldHub');
-  
-        //Starting connection
-        connection.start();
+        this.proxy = connection.createHubProxy('breakingNewsHub');
   
         //Publishing an event when server pushes a message
         this.proxy.on('breakingNews', function (message) {
-            $rootScope.$emit("breakingNews",message);
+            $rootScope.$broadcast("breakingNews", message);
         });
-    };
   
-    return {
-        initialize: initialize
-    }; 
-});
+        console.log('start connection');
+
+        //Starting connection
+        setTimeout(function () {
+            console.log('start');
+            
+            connection.start().done(function () {
+                //Do interesting stuff
+                console.log('started');
+            });
+        }, 1000);
+    };
+})
+.controller('MessagesController', function($scope, breakingNewsService) {
+    breakingNewsService.initialize();
+
+    $scope.messages = [];
+
+     $scope.$on("breakingNews", function (event, message) {
+        $scope.messages.push(message);
+
+        $scope.$apply();
+    });
+}); 
---
+++ server/Ixmucane.BreakingNews/Ixmucane.BreakingNews/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Messaging;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace Ixmucane.BreakingNews
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var hubContext = GlobalHost.ConnectionManager.GetHubContext<BreakingNewsHub>();
+
+            using (var SignalR = WebApp.Start<SignalRStartup>("http://*:8088/"))
+            {
+                while (true)
+                {
+                    var message = Console.ReadLine();
+                    var breakingNews = new BreakingNews(message);
+                    BreakingNewsHub.HubContext.Clients.All.breakingNews(breakingNews);
+                }
+
+                Console.WriteLine("Press [enter] to quit...");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class BreakingNews
+    {
+        public BreakingNews(string message)
+        {
+            Body = message;
+            CreationDate = DateTime.Now.ToString();
+        }
+
+        public string CreationDate { get; set; }
+
+        public string Body { get; set; }
+    }
+    public class SignalRStartup
+    {
+        public static IAppBuilder App = null;
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/signalr", map =>
+            {
+                map.UseCors(CorsOptions.AllowAll);
+
+                var hubConfiguration = new HubConfiguration
+                {
+                    EnableDetailedErrors = true,
+                    EnableJSONP = true
+                };
+
+                map.RunSignalR(hubConfiguration);
+            });
+        }
+    }
+
+    public class BreakingNewsHub : Hub
+    {
+        public BreakingNewsHub()
+        {
+        }
+
+        public override Task OnConnected()
+        {
+            Console.WriteLine("Connection!");
+            return base.OnConnected();
+        }
+
+        public static IHubContext HubContext
+        {
+            get
+            {
+                if (_context == null)
+                    _context = GlobalHost.ConnectionManager.GetHubContext<BreakingNewsHub>();
+
+                return _context;
+            }
+        }
+        static IHubContext _context = null;
+    }
+}
